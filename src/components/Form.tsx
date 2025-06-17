549cb79ed14950f85d22fb27dd88b2e89bf5cd9c
Болотова Екатерина <KateBolotova2003@yandex.ru>
2025-06-17 09:46:22 +0300

    feat: changed discription in form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -183,7 +183,7 @@ const Form = () => {
           <>
             <TextField
               fullWidth
-              label="С кем придете?"
+              label="С кем придете? (Имя Фамилия)"
               name="withWhom"
               value={formData.withWhom}
               onChange={handleChange}
@@ -270,7 +270,7 @@ const Form = () => {
               fullWidth
               multiline
               rows={3}
-              label="Любой комментарий"
+              label="Любой комментарий (Например, одна любимая песня)"
               name="comment"
               value={formData.comment}
               onChange={handleChange}
